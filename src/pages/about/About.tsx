f9c917e3f21827d864fb7c57882794c17efc3903
Douglas Pires <douglas.pires@live.com>
2020-02-03 23:06:04 -0300

    text fixes

--- src/pages/about/About.tsx
+++ src/pages/about/About.tsx
@@ -9,23 +9,26 @@ const About = () => (
         <img src={me} alt="" className="about__me-image d-flex" />
       </div>
       <h1 className="text-center ma-5">Me!</h1>
-      <div className="about__presentation">
+      <div className="about__presentation text-justify">
         <p>
           I'm <strong>Douglas</strong>, nice to meet you! 😀
         </p>
         <br />
-        <p>I'm in love and in touch daily with new excited web technlogies</p>
-        <p>and I absolutely love what I do.</p>
+        <p>
+          I'm in touch with new exciting web technlogies in a daily and I
+          absolutely love what I do.
+        </p>
         <br />
-        <p>My specialty is JavaScript</p>
+        <p>My specialty is JavaScript.</p>
         <br />
         <p>I like to play videogames or watch speedruns ocasionally.</p>
+        <br />
         <p>
-          I am also a great fan of music and I like to play whenever possible
+          I am also a great fan of music and I like to play whenever possible.
         </p>
         <hr className="mt-5 mb-5" />
         <p>
-          Currently living in <strong>Brazil</strong>
+          Currently living in <strong>Brazil</strong>.
         </p>
       </div>
     </div>
