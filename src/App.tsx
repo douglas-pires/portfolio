1cb29707d139a052f4eacd3d59b03e4ae7d9346e
Douglas Pires <douglas.pires@live.com>
2020-02-05 23:05:30 -0300

    add lazy loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,27 @@
-import React from 'react'
+import React, { Suspense, lazy } from 'react'
 import { Router, Route } from 'react-router-dom'
-import Home from './pages/Home'
 import { createBrowserHistory } from 'history'
-import About from './pages/about/About'
 import DNavbar from './components/shared/d-navbar/DNavbar'
-import Projects from './pages/Projects'
+
+const Home = lazy(() => import('./pages/Home'))
+const About = lazy(() => import('./pages/about/About'))
+const Projects = lazy(() => import('./pages/Projects'))
 
 const App = () => (
   <div className="App">
     <Router history={createBrowserHistory()}>
       <DNavbar />
-      <Route exact path="/">
-        <Home />
-      </Route>
-      <Route exact path="/about">
-        <About />
-      </Route>
-      <Route exact path="/projects">
-        <Projects />
-      </Route>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/about">
+          <About />
+        </Route>
+        <Route exact path="/projects">
+          <Projects />
+        </Route>
+      </Suspense>
     </Router>
   </div>
 )
